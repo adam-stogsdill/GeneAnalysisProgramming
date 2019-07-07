6986951d32fdbe8c007e397f362060d702538d3b
Tolmain <adam.stogsdill@gmail.com>
2019-07-06 22:28:37 -0500

    Simply a class created to try all methods. Will use primarily for the use of loading the information and Thread Management.

--- src/bin/main/Main.java
+++ src/bin/main/Main.java
@@ -1,9 +1,34 @@
 package bin.main;
 
+import bin.main.GUI.Window;
+import bin.main.pattern_analysis.Pattern_Statistics;
+
+import javax.swing.*;
+
 public class Main {
 
+    private static int WIDTH = 700;
+    private static int HEIGHT = 500;
+
+
     public static void main(String[] args) {
-        Analytics.N_W_Algorithm(new Gene("GATC"), new Gene("TTACT"));
+        JFrame j = new JFrame("Gene Ally");
+        Window w = new Window(WIDTH, HEIGHT);
+
+        j.setSize(WIDTH, HEIGHT);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setResizable(false);
+        j.add(w);
+
+        j.setVisible(true);
+
+
+        Gene g = new Gene("RACTA");
+
+        //Analytics.N_W_Algorithm(new Gene("GATC"), new Gene("TTACT"));
+        Gene g1 = new Gene(GeneCreation.randomGene(100));
+        Pattern_Statistics unrestricted_size = new Pattern_Statistics(g1);
+        //System.out.println(unrestricted_size.toString());
     }
 
 }
