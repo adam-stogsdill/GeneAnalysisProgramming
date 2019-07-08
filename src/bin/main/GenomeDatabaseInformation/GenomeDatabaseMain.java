a0fe275c22e10b3e99cb5f519c2f587c903b60ce
Tolmain <adam.stogsdill@gmail.com>
2019-07-07 21:59:20 -0500

    Added some more GenomRange Information manually.

--- src/bin/main/GenomeDatabaseInformation/GenomeDatabaseMain.java
+++ src/bin/main/GenomeDatabaseInformation/GenomeDatabaseMain.java
@@ -18,7 +18,24 @@ public class GenomeDatabaseMain {
             ranges.add(new GenomeRanges(1, 'p', 3, 6, 3, 1, 7100000));
             ranges.add(new GenomeRanges(1, 'p', 3, 6, 2, 7100001, 15900000));
             ranges.add(new GenomeRanges(1, 'p', 3, 6, 1, 15900001, 27600000));
-            ranges.add(new GenomeRanges())
+            ranges.add(new GenomeRanges(1, 'p', 3, 5, 0, 27600001, 34300000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 4, 3, 34300001, 39600000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 4, 2, 39600001, 43700000));
+            ranges.add(new GenomeRanges(1, 'P', 3, 4, 1, 43700001, 46300000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 3, 0, 46300001, 50200000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 2, 3, 50200000, 55600000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 2, 2, 55600001, 58500000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 2, 1, 58500001, 60800000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 1, 3, 60800001, 68500000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 1, 2, 68500001, 69300000));
+            ranges.add(new GenomeRanges(1, 'p', 3, 1, 1, 69300001, 84400000));
+            ranges.add(new GenomeRanges(1, 'p', 2, 2, 3, 84000001, 87900000));
+            ranges.add(new GenomeRanges(1, 'p', 2, 2, 2, 87900001, 91500000));
+            ranges.add(new GenomeRanges(1, 'p', 2, 2, 1, 91500001, 94300000));
+            ranges.add(new GenomeRanges(1, 'p', 2, 1, 0, 94300001, 10670000));
+            //ranges.add(new GenomeRanges())
+
+
         } catch (GenomeDatabaseError genomeDatabaseError) {
             genomeDatabaseError.printStackTrace();
         }
